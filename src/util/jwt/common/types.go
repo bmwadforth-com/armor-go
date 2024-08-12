7c48023d8813fe744c0c77e39a49312265d9fe27
Brannon <16732831+bmwadforth@users.noreply.github.com>
2024-08-12 23:58:06 +1000

    Update release (#14)
    * feat: add jwe
    
    * feat: add jwe
    
    * feat: add jwe

--- src/util/jwt/common/types.go
+++ src/util/jwt/common/types.go
@@ -65,8 +65,24 @@ type Token struct {
 	Raw    []byte
 }
 
+// TokenInstance represents an interface for working with tokens.
+// TokenInstance will either be an instance of a JWE or JWS token.
 type TokenInstance interface {
+	// Encode generates a serialized representation of the token, typically in a compact format like JWE or JWT.
+	// It returns the encoded token as a byte slice and any potential errors encountered during encoding.
 	Encode() ([]byte, error)
+
+	// Decode parses a serialized token (split into its parts) and populates the internal token structure.
+	// For JWS, the header, payload, SignFunc and ValidateFunc are all populated (along with Raw byte values).
+	// For JWE, the header, encryptedKey, initialization vector, cipher text, SignFunc and Validate func are
+	// all populated (along with Raw byte values).
+	// It takes the token parts as input and returns an error if the decoding or parsing process fails.
 	Decode(parts []string) error
+
+	// Validate verifies the integrity and authenticity of the token, checking signatures, claims, and expiration if applicable.
+	// Uniquely for JWE, it is only after the call to Validate that the payload data is populated into the token struct. This is
+	// because the payload is not available when calling Decode like it is for a JWS. It is only after Validating the token that
+	// the 'cipher text' is decrypted to reveal the contents of the payload (which can be JSON, or any other data).
+	// It returns a boolean indicating whether the token is valid and any potential errors encountered during validation.
 	Validate() (bool, error)
 }
